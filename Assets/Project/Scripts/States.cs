8176816b24f532c45d6b47d10c42ecf1e58204cc
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2025-09-17 19:20:46 +0200

    Remove GitAmend based controller

---
+++ Assets/Project/Scripts/States.cs
@@ -0,0 +1,78 @@
+﻿using UnityUtils.StateMachine;
+
+
+public class GroundedState : IState
+{
+    private readonly PlayerController _controller;
+
+    public GroundedState(PlayerController controller)
+    {
+        _controller = controller;
+    }
+
+    public void OnEnter()
+    {
+        _controller.OnGroundContactRegained();
+    }
+}
+
+public class FallingState : IState
+{
+    private readonly PlayerController _controller;
+
+    public FallingState(PlayerController controller)
+    {
+        _controller = controller;
+    }
+
+    public void OnEnter()
+    {
+        _controller.OnFallStart();
+    }
+}
+
+public class SlidingState : IState
+{
+    private readonly PlayerController _controller;
+
+    public SlidingState(PlayerController controller)
+    {
+        _controller = controller;
+    }
+
+    public void OnEnter()
+    {
+        _controller.OnGroundContactLost();
+    }
+}
+
+public class RisingState : IState
+{
+    private readonly PlayerController _controller;
+
+    public RisingState(PlayerController controller)
+    {
+        _controller = controller;
+    }
+
+    public void OnEnter()
+    {
+        _controller.OnGroundContactLost();
+    }
+}
+
+public class JumpingState : IState
+{
+    private readonly PlayerController _controller;
+
+    public JumpingState(PlayerController controller)
+    {
+        _controller = controller;
+    }
+
+    public void OnEnter()
+    {
+        _controller.OnGroundContactLost();
+        _controller.OnJumpStart();
+    }
+}
