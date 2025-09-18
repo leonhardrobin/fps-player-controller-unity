4ad2bd83e27cb717ff6ed8af527ad9066f45321f
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2025-09-18 11:34:20 +0200

    Update debugger

--- Assets/Project/Scripts/ControllerDebugger.cs
+++ Assets/Project/Scripts/ControllerDebugger.cs
@@ -5,11 +5,20 @@ public class ControllerDebugger : MonoBehaviour
 {
     [SerializeField] private PlayerController _controller;
 
+    private float _topSpeedHorizontal;
+    private float _topSpeedVertical;
+
     private void OnGUI()
     {
-        string horVelString = VectorMath.RemoveDotVector(_controller.GetVelocity(), Vector3.up).magnitude.ToString("F");
-        string verVelString = VectorMath.ExtractDotVector(_controller.GetVelocity(), Vector3.up).magnitude.ToString("F");
-        GUI.Label(new Rect(50f, 50f, 200f, 20f), $"H: {horVelString}");
-        GUI.Label(new Rect(50f, 80f, 200f, 20f), $"V: {verVelString}");
+        float horVel = VectorMath.RemoveDotVector(_controller.GetVelocity(), Vector3.up).magnitude;
+        float verVel = VectorMath.ExtractDotVector(_controller.GetVelocity(), Vector3.up).magnitude;
+
+        if (Mathf.Abs(horVel) > _topSpeedHorizontal) _topSpeedHorizontal = horVel;
+        if (Mathf.Abs(verVel) > _topSpeedVertical) _topSpeedVertical = verVel;
+        
+        GUI.Label(new Rect(50f, 50f, 200f, 20f), $"H: {horVel:F}");
+        GUI.Label(new Rect(50f, 80f, 200f, 20f), $"^H: {_topSpeedHorizontal:F}");
+        GUI.Label(new Rect(50f, 110f, 200f, 20f), $"V: {verVel:F}");
+        GUI.Label(new Rect(50f, 140f, 200f, 20f), $"^V: {_topSpeedVertical:F}");
     }
 }
